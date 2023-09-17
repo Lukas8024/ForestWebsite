40285ae8beccb342ca76aa06f317496fa90d7bb3
Lukas <lukas802480@gmail.com>
2023-09-17 11:30:46 +0200

    change nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,20 @@
-const burgerBtn = document.querySelector('.nav-button')
 const navMobile = document.querySelector('.nav-mobile__items')
+const burgerBtn = document.querySelector('.nav-button')
+const iconBtn = document.querySelector('#icon')
 
-const handleNav = ()=>{
-    navMobile.classList.toggle('nav-mobile__items--active')
+const handleNav = () => {
+	navMobile.classList.toggle('nav-mobile__items--active')
+	changeIcon()
+}
+
+const changeIcon = () => {
+	if (navMobile.classList.contains('nav-mobile__items--active')) {
+		iconBtn.classList.remove('fa-solid', 'fa-bars')
+		iconBtn.classList.add('fa-solid', 'fa-xmark')
+	} else {
+		iconBtn.classList.remove('fa-solid', 'fa-xmark')
+		iconBtn.classList.add('fa-solid', 'fa-bars')
+	}
 }
 
 burgerBtn.addEventListener('click', handleNav)
