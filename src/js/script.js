c4b42969ae07379380786358d9c55d251a3b0307
Lukas <lukas802480@gmail.com>
2023-09-27 15:08:02 +0200

    add srollspy

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,25 @@
 const navMobile = document.querySelector('.nav-mobile__items')
 const burgerBtn = document.querySelector('.nav-button')
 const iconBtn = document.querySelector('#icon')
+const allNavItems = document.querySelectorAll('.nav-mobile__item')
 const footerYear = document.querySelector('.footer__year')
+const body = document.querySelector('body')
+
+let section = document.querySelectorAll('.section-scroll')
+let navLinks = document.querySelectorAll('.nav-desktop__item')
 
 const handleNav = () => {
 	navMobile.classList.toggle('nav-mobile__items--active')
+
+	body.style.overflow = 'hidden'
+	
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav-mobile__items--active')
+			changeIcon()
+			body.style.overflow = 'auto'
+		})
+	})
 	changeIcon()
 }
 
@@ -23,6 +38,22 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year
 }
 
-handleCurrentYear()
+window.onscroll = () => {
+	section.forEach(sec => {
+		let top = window.scrollY
+		let offset = sec.offsetTop - 120
+		let height = sec.offsetHeight
+		let id = sec.getAttribute('id')
+
+		if (top >= offset && top < offset + height) {
+			navLinks.forEach(links => {
+				links.classList.remove('nav-desktop__item--active')
+				document.querySelector('.nav-desktop__items a[href*=' + id + ']').classList.add('nav-desktop__item--active')
+			})
+		}
+	})
+}
 
 burgerBtn.addEventListener('click', handleNav)
+
+handleCurrentYear()
