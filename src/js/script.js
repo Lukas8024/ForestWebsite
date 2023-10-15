6c985ae0b5c750ceb2667bb25b2a7e139f8b406e
Lukas <lukas802480@gmail.com>
2023-10-15 17:11:02 +0200

    change scrolln spy

--- src/js/script.js
+++ src/js/script.js
@@ -39,22 +39,5 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year
 }
 
-window.onscroll = () => {
-	section.forEach(sec => {
-		let top = window.scrollY
-		let offset = sec.offsetTop - 100
-		let height = sec.offsetHeight
-		let id = sec.getAttribute('id')
-		
-		if (top >= offset && top < offset + height) {
-			navLinks.forEach(links => {
-				links.classList.remove('nav-desktop__item--active')
-				document.querySelector('.nav-desktop__items a[href*=' + id + ']').classList.add('nav-desktop__item--active')
-			})
-		}
-	})
-}
-
-burgerBtn.addEventListener('click', handleNav)
 handleCurrentYear()
-
+burgerBtn.addEventListener('click', handleNav)
---
+++ src/js/scroll_spy.js
@@ -0,0 +1,15 @@
+window.onscroll = () => {
+	section.forEach(sec => {
+		let top = window.scrollY
+		let offset = sec.offsetTop - 100
+		let height = sec.offsetHeight
+		let id = sec.getAttribute('id')
+		
+		if (top >= offset && top < offset + height) {
+			navLinks.forEach(links => {
+				links.classList.remove('nav-desktop__item--active')
+				document.querySelector('.nav-desktop__items a[href*=' + id + ']').classList.add('nav-desktop__item--active')
+			})
+		}
+	})
+}
